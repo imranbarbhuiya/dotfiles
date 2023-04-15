d2c7d820081e7126c8d36958aaf5153f128d70e3
Parbez <imranbarbhuiya.fsd@gmail.com>
2023-04-15 14:55:42 +0530

    add gh copilot cli alias

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -31,7 +31,6 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 & "$completionsDirectory\Gh-Completions.ps1"
 & "$completionsDirectory\Custom-Completions.ps1"
 & "$completionsDirectory\Volta-Completions.ps1"
-& "$completionsDirectory\Git-Cliff-Completions.ps1"
 
 
 # Load all custom aliases
@@ -39,3 +38,40 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 $Env:STARSHIP_CONFIG = "$PSScriptRoot\starship.toml"
 Invoke-Expression (&starship init powershell)
+
+# github copilot cli
+Function copilot_check-response {
+  if ( $Args[0] -eq $true ) {
+    $FIXED_CMD = Get-Content $TMPFILE
+    Remove-Item $TMPFILE -ErrorAction SilentlyContinue
+    Invoke-Expression $FIXED_CMD
+  }
+  else {
+    Write-Host "User cancelled the command."
+    Remove-Item $TMPFILE -ErrorAction SilentlyContinue
+  }
+}
+
+Function copilot_what-the-shell {
+  $TMPFILE = New-TemporaryFile
+  trap {Remove-Item $TMPFILE -ErrorAction SilentlyContinue}
+  github-copilot-cli what-the-shell "$args" --shellout $TMPFILE
+  copilot_check-response $?
+}
+Set-Alias ?? copilot_what-the-shell
+
+Function copilot_git-assist {
+  $TMPFILE = New-TemporaryFile
+  trap {Remove-Item $TMPFILE -ErrorAction SilentlyContinue}
+  github-copilot-cli git-assist "$args" --shellout $TMPFILE
+  copilot_check-response $?
+}
+Set-Alias git? copilot_git-assist
+
+Function copilot_gh-assist {
+  $TMPFILE = New-TemporaryFile
+  trap {Remove-Item $TMPFILE -ErrorAction SilentlyContinue}
+  github-copilot-cli gh-assist "$args" --shellout $TMPFILE
+  copilot_check-response $?
+}
+Set-Alias gh? copilot_gh-assist

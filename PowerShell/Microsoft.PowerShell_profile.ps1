a675639e5816b23fe387fb8940591ec1cf96c4fe
Parbez <imranbarbhuiya.fsd@gmail.com>
2024-10-16 01:03:56 +0530

    update config

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 $functionsDirectory = "$PSScriptRoot\functions";
-$completionsDirectory = "$PSScriptRoot\completions";
+# $completionsDirectory = "$PSScriptRoot\completions";
 $aliasesDirectory = "$PSScriptRoot\aliases";
 
 Import-Module -Name posh-git
@@ -23,13 +23,14 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 . $functionsDirectory\GitHub-Cli-Functions.ps1
 . $functionsDirectory\Github-Copilot.ps1
 . $functionsDirectory\NodeJS-Functions.ps1
+. $functionsDirectory\Scilab-Functions.ps1
 . $functionsDirectory\Terminal-Functions.ps1
 
 # Load all custom completions
-& "$completionsDirectory\Gh-Completions.ps1"
-& "$completionsDirectory\Git-Cliff-Completions.ps1"
-& "$completionsDirectory\Starship-Completions.ps1"
-& "$completionsDirectory\Volta-Completions.ps1"
+# & "$completionsDirectory\Gh-Completions.ps1"
+# & "$completionsDirectory\Git-Cliff-Completions.ps1"
+# & "$completionsDirectory\Starship-Completions.ps1"
+# & "$completionsDirectory\Volta-Completions.ps1"
 
 
 # Load all custom aliases
@@ -37,4 +38,4 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 $Env:STARSHIP_CONFIG = "$PSScriptRoot\starship.toml"
 Invoke-Expression (&starship init powershell)
-
+fnm env --use-on-cd --corepack-enabled | Out-String | Invoke-Expression
